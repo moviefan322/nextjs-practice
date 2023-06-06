39a42df7d1071d6f1b7758c555cf4e1f1990c29b
moviefan322 <philman202@yahoo.com>
2023-06-05 20:37:49 -0400

    adds programmatic navigation for client projects

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -7,9 +7,18 @@ function ClientProjectsPage(): JSX.Element {
   console.log(router.query.id);
   const query: string | string[] | undefined = router.query.id;
 
+  function loadProjectHandler(): void {
+    // load data...
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: query, clientprojectid: "projecta" },
+    });
+  }
+
   return (
     <div>
       <h1>Projects of {query}</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
