6004ef882f5178d21b8d646dfae0afe21259d958
moviefan322 <philman202@yahoo.com>
2023-06-05 20:13:33 -0400

    extracts dynamic data from pathname

--- pages/portfolio/[projectId].tsx
+++ pages/portfolio/[projectId].tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import { useRouter, NextRouter } from "next/router";
 
 function PortfolioProjectPage() {
+  const router: NextRouter = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
+
+  // Send a request to some backend server
+  // to fetch the piece of data with an id of router.query.projectId
+
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
