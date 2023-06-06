55e50090dc79fdc5d35bfe911be6a6c76b8e4e10
moviefan322 <philman202@yahoo.com>
2023-06-05 20:33:07 -0400

    dynamically renders mapped routes

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -4,11 +4,12 @@ function ClientProjectsPage(): JSX.Element {
   const router: NextRouter = useRouter();
 
   console.log(router.pathname);
-  console.log(router.query);
-  
+  console.log(router.query.id);
+  const query: string | string[] | undefined = router.query.id;
+
   return (
     <div>
-      <h1>Projects of A Given Client</h1>
+      <h1>Projects of {query}</h1>
     </div>
   );
 }
