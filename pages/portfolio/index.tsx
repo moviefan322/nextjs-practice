6dd53005454d3d9437091f34bd83a2841621d95f
moviefan322 <philman202@yahoo.com>
2023-06-05 20:06:22 -0400

    adds Portfolio path

---
+++ pages/portfolio/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PortfolioPage(): JSX.Element {
+  return (
+    <div>
+      <h1>The Portfolio Page</h1>
+    </div>
+  );
+}
+
+export default PortfolioPage;
